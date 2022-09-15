57ed19d9f12c93e99372cba18f44d070b54ca4c8
abhiraj77 <abhirajchaudhary77@gmail.com>
2022-09-15 14:24:15 +0530

    binary tree traversal updated

--- Tree Based DSA/Binary Tree.py
+++ Tree Based DSA/Binary Tree.py
@@ -4,6 +4,7 @@ class Node:
         self.right = None
         self.data = data
 
+    # Print the Tree
     def PrintTree(self):
         if self.left:
             self.left.PrintTree()
@@ -11,6 +12,7 @@ class Node:
         if self.right:
             self.right.PrintTree()
 
+    # Insert Node
     def insert(self, data):
         if self.data:
             if data < self.data:
@@ -25,9 +27,45 @@ class Node:
                     self.right = Node(data)
         else:
             self.data = data
+    
+    # Inorder Traversal 
+    # Left->Root->Right
+    def Inorder(self, root):
+        result = []
+        if root:
+            result = self.Inorder(root.left)
+            result.append(root.data)
+            result = result + self.Inorder(root.right)
+            print(result)
+        return result
 
-root = Node(12)
-root.insert(6)
+    # Preorder Traversal
+    # Root->Left->Right
+    def Preorder(self,root):
+        result = []
+        if root:
+            result.append(root.data)
+            result = result + self.Preorder(root.left)
+            result = result + self.Preorder(root.right)
+        return result
+    
+    # Postorder Traversal
+    # Left->Right->Root
+    def Postorder(self,root):
+        result = []
+        if root:
+            result = self.Postorder(root.left)
+            result = result + self.Postorder(root.right)
+            result.append(root.data)
+        return result
+
+root = Node(27)
 root.insert(14)
-root.insert(3)
-root.PrintTree()
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+print(f"Inorder = {root.Inorder(root)}")
+print(f"Preorder = {root.Preorder(root)}")
+print(f"Postorder = {root.Postorder(root)}")
